de5f8194015c03b65af1cf8b7034df2196b01cad
unknown <adipradipta93@gmail.com>
2017-09-21 11:51:59 +0700

    perdana

---
+++ ketinggian_air.ino
@@ -0,0 +1,109 @@
+#include <SoftwareSerial.h>
+#define ultrasonic_trigger_pin 8
+#define ultrasonic_echo_pin 9
+#define NOTIF_TINGGI 1
+#define NOTIF_NORMAL 2
+#define NOTIF_RENDAH 3
+#define NOTIF_OFF 4
+
+const byte rxPin = 6;
+const byte txPin = 7;
+
+//----Var untuk ultarsonic
+float lokasi_sensor = 35;
+float batas_atas = lokasi_sensor - 20;
+float batas_bawah = 0;
+
+
+SoftwareSerial ESP8266 (rxPin, txPin);
+
+int counter_tinggi = 0;
+int counter_normal = 0;
+int counter_rendah = 0;
+
+String notif = "";
+
+void setup() {
+  pinMode(ultrasonic_trigger_pin, OUTPUT);
+  pinMode(ultrasonic_echo_pin, INPUT);
+  Serial.begin(115200);   
+  ESP8266.begin(115200);
+}
+
+void loop(){
+  String data = bacaKetinggian();
+  String server = "http://192.168.43.3/automation_system_adi/index.php/api/post_data/" + data;
+  ESP8266.print(server);
+  delay(1000);
+  int ctr = 1;
+  //do{
+    Serial.println(ctr);
+    if(ESP8266.available()){
+      String response = ESP8266.readString();
+      Serial.println(response);
+    }
+    ctr++;  
+    //delay(1000);
+  //}while(!ESP8266.available() || ctr >=15);
+  delay(1000);
+}
+
+String bacaKetinggian(){
+  Serial.println("Menjalankan Fungsi bacaKetinggian()");
+  long duration, distance;
+  int ketinggian_air;
+  digitalWrite(ultrasonic_trigger_pin, LOW);  
+  delayMicroseconds(2); 
+  digitalWrite(ultrasonic_trigger_pin, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(ultrasonic_trigger_pin, LOW);
+  duration = pulseIn(ultrasonic_echo_pin, HIGH);
+  distance = (duration/2) / 29.1;
+  Serial.print("Sensor diletakan di ketinggian : ");
+  Serial.println(lokasi_sensor); 
+  ketinggian_air = (lokasi_sensor - distance) - 0;
+  Serial.print("Ketinggian air saat ini :  ");
+  Serial.println(ketinggian_air);
+  String status_ketinggian = "";
+  //fuzzylogic
+  //untuk kondisi Tinggi
+  if (ketinggian_air >= batas_atas){
+    counter_tinggi ++;
+    counter_normal = 0 ;
+    counter_rendah = 0 ;
+    status_ketinggian = "Tinggi";
+    Serial.println("Ketinggian air sudah masuk batas atas, mohon untuk dibuka pintu Bendungan");
+  }else
+  //untuk kondisi normal
+  if (ketinggian_air < batas_atas && ketinggian_air > (batas_atas - 10)){
+    counter_tinggi = 0;
+    counter_normal ++ ;
+    counter_rendah = 0 ;
+    status_ketinggian = "Normal";
+    Serial.println("Ketinggian air kondisi normal");
+  }else 
+  //untuk kodisi rendah 
+  if (ketinggian_air <= (batas_atas - 10)){
+    counter_tinggi = 0;
+    counter_normal = 0 ;
+    counter_rendah ++ ;
+    status_ketinggian = "Rendah";
+    Serial.println("Ketinggian air kondisi surut, mohon untuk ditutup puntu Bendungan");
+  }
+  delay(3000);  
+  if(counter_tinggi >= 10){
+      notif = NOTIF_TINGGI;
+  }else
+  if(counter_normal >= 10){
+      notif = NOTIF_NORMAL;
+  }else
+  if(counter_rendah >= 10){
+      notif = NOTIF_RENDAH;
+  }else{
+    notif = NOTIF_OFF;
+  }
+  
+  String retVal = (String)ketinggian_air +"/"+ status_ketinggian +"/";
+  return retVal;
+}
+ 
